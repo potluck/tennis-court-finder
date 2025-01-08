d7c9b8940bbe453f2817fbec0d8063483bb316bd
Potluck Mittal <potluck.mittal@gmail.com>
2025-01-07 20:30:33 -0500

    remove log

--- pages/index.tsx
+++ pages/index.tsx
@@ -94,7 +94,6 @@ export default function Home() {
   }, [router.isReady, router.query.includeHalfHourSlots]);
 
 
-  console.log("hey pots... ", timeSlots[0]);
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
